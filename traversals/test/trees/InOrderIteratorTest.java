1b40fe9e779ddc28925f08757bb4367908745792
Aniol0012 <aniol0012@gmail.com>
2023-12-21 10:50:06 +0100

    tests: add task 2 and 3 tests

--- traversals/test/trees/InOrderIteratorTest.java
+++ traversals/test/trees/InOrderIteratorTest.java
@@ -10,5 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InOrderIteratorTest extends AbstractLinkedBinaryTreeTest {
 
-    // TODO: Exercises 2 & 3
+    @Test
+    @DisplayName("In-order traversal of a non-empty tree")
+    void testInOrderTraversalNonEmpty() {
+        var expected = List.of(2, 4, 1, 5, 3);
+        assertEquals(expected, iterate(tree.inOrderIterator()), "In-order traversal should match expected sequence for a non-empty tree");
+    }
+
+    @Test
+    @DisplayName("In-order traversal of an empty tree")
+    void testInOrderTraversalEmpty() {
+        assertTrue(iterate(empty.inOrderIterator()).isEmpty(), "In-order traversal of an empty tree should be empty");
+    }
+
+    @Test
+    @DisplayName("hasNext and next methods")
+    void testHasNextAndNext() {
+        var iterator = tree.inOrderIterator();
+        assertTrue(iterator.hasNext(), "hasNext should return true for a non-empty tree");
+        assertEquals(Integer.valueOf(2), iterator.next(), "Next should return the first element in in-order sequence");
+    }
+
+    @Test
+    @DisplayName("Exception handling for next method")
+    void testNextException() {
+        var iterator = empty.inOrderIterator();
+        assertFalse(iterator.hasNext(), "hasNext should return false for an empty tree");
+        assertThrows(NoSuchElementException.class, iterator::next, "Calling next on an empty iterator should throw NoSuchElementException");
+    }
 }
