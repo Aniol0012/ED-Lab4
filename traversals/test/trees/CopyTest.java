5675c87b5f23d37e8efb5eee5c5ceeb2ff6e9f7a
Aniol0012 <aniol0012@gmail.com>
2023-12-21 20:02:10 +0100

    made copy test useful

--- traversals/test/trees/CopyTest.java
+++ traversals/test/trees/CopyTest.java
@@ -2,6 +2,7 @@ package trees;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.support.hierarchical.Node;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -9,18 +10,13 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 class CopyTest extends AbstractLinkedBinaryTreeTest {
 
     @Test
-    @DisplayName("Test copy constructor creates a copy of the tree")
-    void testCopyConstructor() {
+    @DisplayName("Copy of a non-empty tree is equal but independent")
+    void testCopy() {
         LinkedBinaryTree<Integer> copyTree = new LinkedBinaryTree<>(tree);
 
-        // Verify that the copy is not the same instance as the original
-        assertEquals(tree, copyTree, "The copy of the tree must be a different instance");
+        assertEquals(tree, copyTree, "The copy should be equal to the original");
 
-        // Verify that the structure and values are the same
-        assertEquals(iterate(tree.inOrderIterator()), iterate(copyTree.inOrderIterator()), "The trees must have the same structure and values");
-
-        // Modify the copy and verify that the original does not change
-        copyTree.setRoot(10);
-        assertNotEquals(tree.root(), copyTree.root(), "Modify the copy should not affect the original tree");
+        copyTree.setRoot(99);
+        assertNotEquals(tree.root(), copyTree.root(), "Modifying the copy should not affect the original");
     }
 }
